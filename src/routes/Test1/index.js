a127262e541ef724f802cc29024c7f2666693685
Moch Arief Febriansyah <arief@radyalabs.id>
2023-08-11 23:14:17 +0700

    feat: done test 1

--- src/routes/Test1/index.js
+++ src/routes/Test1/index.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
+
 const Test1 = () => {
+  const [userInput, setUserInput] = useState(null)
+
   return(
     <div>
       <ul>
         <li>Please render the inputted value</li>
       </ul>
       <p>
-        <b>User Input: </b> ______
+        <b>User Input: </b> {userInput}
       </p>
-      <input type="text" placeholder="input here"/>
+      <input type="text" placeholder="input here" onChange={(e) => { setUserInput(e.currentTarget.value) }}/>
     </div>
   )
 }
