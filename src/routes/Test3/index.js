464d5dc17d57e9e3177617ab70fd71a1ec5594bd
Moch Arief Febriansyah <arief@radyalabs.id>
2023-09-08 13:14:17 +0700

    feat: done test 3

--- src/routes/Test3/index.js
+++ src/routes/Test3/index.js
@@ -1,13 +1,17 @@
+import { useRef } from "react";
+
 const Test3 = () => {
+  const inputFocus = useRef(null);
+  
   return(
     <div>
       <ul>
         <li>Please focus to the text field when user click the focus button</li>
       </ul>
       <p>
-        <button type="button">Focus Text Field</button>
+        <button type="button" onClick={() => { inputFocus.current.focus() }}>Focus Text Field</button>
       </p>
-      <input type="text" placeholder="focus me"/>
+      <input type="text" placeholder="focus me" ref={inputFocus}/>
     </div>
   )
 }
